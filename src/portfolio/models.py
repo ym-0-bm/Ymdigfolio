2116d2fde539c9c0d6b04642524050e27d8b7f24
ym-0-bm <93938113+ym-0-bm@users.noreply.github.com>
2024-03-02 14:03:04 +0000

    mise en place de l'environnement django

---
+++ src/portfolio/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    name = models.fields.CharField(max_length=100)
+
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2024)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
